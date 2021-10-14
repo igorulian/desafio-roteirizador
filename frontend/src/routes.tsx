207991f2cb3d5eb16ac9890eceeb2a17dfb0511b
Igor Ulian <igorulian@outlook.com>
2021-10-14 15:36:26 -0300

    add PrivateRoute

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect, RouteProps } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+
+const PrivateRoute = ({ ...rest }: RouteProps) => {
+  const isAuthenticated = !!localStorage.getItem('token')
+
+  return isAuthenticated ? <Route {...rest}/> : <Redirect to={{ pathname: '/login' }} />
+}
 
 const Routes:React.FC = () => {
   return (
     <Switch>
         <Route exact path = '/login' component={Login}/>
+        <PrivateRoute exact path = '/' component={Dashboard} />
     </Switch>
   )
 }
